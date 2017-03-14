42ae9b7c7420e70c92801de29cafa8eac7dbd76f
Ming ZHAO <mingzhao.pro@gmail.com>
2017-03-14 01:40:29 +0100

    better solution for calculator

--- week4/calculator/src/main/scala/calculator/Calculator.scala
+++ week4/calculator/src/main/scala/calculator/Calculator.scala
@@ -11,34 +11,31 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    for(ex <- namedExpressions) yield (ex._1, new Signal(eval(new Ref(ex._1), namedExpressions)))
+    for(ex <- namedExpressions) yield (ex._1, new Signal(eval(ex._2(), namedExpressions)))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
 
-    def calculate(e: Expr, lastE: Expr): Double = {
-      e match {
-        case Literal(v) => v
-        case Ref(n) => {
-          val newExpr = getReferenceExpr(n, references)
-          if (newExpr == Literal(Double.NaN) || lastE.equals(newExpr)) Double.NaN
-          else calculate(newExpr, e)
+    def calculate(e: Expr, usedFields: List[Expr]): Double = {
+        e match {
+          case Literal(v) => v
+          case Ref(n) => {
+            val newExpr = getReferenceExpr(n, references)
+            if(usedFields contains newExpr) Double.NaN
+            else calculate(newExpr, newExpr :: usedFields)
+          }
+          case Plus(a, b) => calculate(a, a :: b :: usedFields) + calculate(b, a :: b :: usedFields)
+          case Minus(a, b) => calculate(a, a :: b :: usedFields) - calculate(b, a :: b :: usedFields)
+          case Times(a, b) => calculate(a, a :: b :: usedFields) * calculate(b, a :: b :: usedFields)
+          case Divide(a, b) => calculate(a, a:: b :: usedFields) / calculate(b, a :: b :: usedFields)
         }
-        case Plus(a, b)  => {
-//          println("e " + e + " a " + a + " lastE " + lastE)
-          if(e.toString.contains(a.toString)
-            || e.toString.contains(b.toString)
-            || lastE.toString.equals(a.toString)
-            || lastE.toString.equals(b.toString))
-            Double.NaN
-          else calculate(a, a) + calculate(b, b)
-        }
-        case Minus(a, b) => calculate(a, a) - calculate(b, b)
-        case Times(a, b) => calculate(a, a) * calculate(b, b)
-        case Divide(a, b) => calculate(a, a) / calculate(b, b)
+
+      def cal(a: Expr, b: Expr, fields: List[Expr], f:(Double, Double) => Double): Double = {
+        if((fields contains a) || (fields contains b)) Double.NaN
+        else f(a, b)
       }
     }
-    calculate(expr, expr)
+    calculate(expr, List())
   }
 
   /** Get the Expr for a referenced variables.
